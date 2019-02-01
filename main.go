d8eb282cd6b5f0891f923c025f87b031bc07d7f7
Rémi Dubois <packman@oxiame.net>
2019-02-01 14:23:12 +0100

    Initialize new Beego project

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	_ "gitlab.com/postflight/api/routers"
+
+	"github.com/astaxie/beego"
+)
+
+func main() {
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.Run()
+}
